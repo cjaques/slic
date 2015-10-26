dc6fad489c4e044d554fe9bfcdbad1971b5f66de
ElPhasmoLoco <christian.jaques@gmail.com>
2015-10-26 10:26:30 +0100

    Corrected inverted axis from numpy to c++ (image was turned 90 degrees)

--- slicmodule.cpp
+++ slicmodule.cpp
@@ -9,23 +9,28 @@
 #include "LKM.h"
 #include "utils.h"
 
+#include "opencv2/core/core.hpp"
+#include "opencv2/highgui/highgui.hpp"
+
+// #include "cv.h"
+// #include "highgui.h"
 // #define DEBUG
 
 using namespace vigra;
 
 static PyObject * slic_NumpyArgs(PyObject *self, PyObject *args)
 {
-  PyObject *list2_obj;
-  PyObject *list3_obj;
+  PyObject *inputArray;
+
   int dimX;
   int dimY;
   int STEP;
   float M;
-  if (!PyArg_ParseTuple(args, "Oiiif", &list2_obj, &dimX, &dimY,&STEP,&M)) // Getting arrays in PyObjects
+  if (!PyArg_ParseTuple(args, "Oif", &inputArray,&STEP,&M)) // Getting arrays in PyObjects
     return NULL;
 
   #ifdef DEBUG
-  printf("[slicmodule.cpp] Arrays dimensions : x: %d, y: %d \nSuperpixel parameters : STEP: %d, M: %d",dimX,dimY,STEP,M);
+  printf("[slicmodule.cpp] Arrays dimensions : x: %d, y: %d \nSuperpixel parameters : STEP: %d, M: %f\n",dimX,dimY,STEP,M);
   #endif
 
   double ***inputVolume; // 3dims double array
@@ -36,38 +41,45 @@ static PyObject * slic_NumpyArgs(PyObject *self, PyObject *args)
   PyArray_Descr *descr;
   descr = PyArray_DescrFromType(typenum);
   npy_intp dims[3];
-  if (PyArray_AsCArray(&list2_obj, (void ***)&inputVolume, dims, 3, descr) < 0 ){ //|| PyArray_AsCArray(&list3_obj, (void ***)&list3, dims, 3, descr) < 0) {
+  if (PyArray_AsCArray(&inputArray, (void ***)&inputVolume, dims, 3, descr) < 0 ){ 
     PyErr_SetString(PyExc_TypeError, "[slicmodule.cpp] Error converting to c array");
     return NULL; 
   }
 
   int numlabels = 100;
   // TODO CHris - how to avoid this? Creation of unnecessary new array
+  dimX = dims[0];
+  dimY = dims[1];
   int imgLength = dimX*dimY;
   UINT* ubuff = new UINT[imgLength];
   UINT* outbuff = new UINT[imgLength];
   sidType* labels = new sidType[imgLength]; 
   LKM lkm;
 
-  int idx =0;
-  for(int i=0;i<dimY;i++)
-    for(int j=0;j<dimX;j++)
-    {
-      ubuff[idx] = inputVolume[i][j][0]; 
-      idx++;
-    }
-
   #ifdef DEBUG
+  // The following lines copy and save the input image. Used to debug inverted axis from numpy to C++
+  // idx1=0;
+  // cv::Mat im2 = cv::Mat::zeros(dimX,dimY, CV_8UC1);
+  // for(int i=0;i<dimY;i++)
+  // {
+  //   uchar* rowi = im2.ptr(i);
+  //   for(int j=0;j<dimX;j++) 
+  //     {
+  //       rowi[j] = ubuff[idx1];
+  //       idx1 ++;
+  //     }
+  // }
+  // cv::imwrite("/Users/Chris/Code/Images/deeeeAfterContours.jpg",im2);
   printf("[slicmodule.cpp] Generating superpixels. STEP=%d, M=%f\n", STEP, M);
   #endif
   lkm.DoSuperpixelSegmentation(ubuff, dimX, dimY, labels, numlabels, STEP, M);
-  
-  UINT color = 0xff0000;
+
+  UINT color = 0xff0000; //0xff0000 draws red contours
   
   #ifdef DEBUG
   printf("[slicmodule.cpp] Drawing contours around segments ...\n");
   #endif
-  DrawContoursAroundSegments(ubuff, labels, dimX, dimY,  color);//0xff0000 draws red contours
+  DrawContoursAroundSegments(ubuff, labels, dimX, dimY, color);
   
   dims[0] = dimX;
   dims[1] = dimY;
@@ -78,13 +90,15 @@ static PyObject * slic_NumpyArgs(PyObject *self, PyObject *args)
   PyObject* ret = PyArray_SimpleNew(3, dims, NPY_DOUBLE);
   PyArrayObject * returnval = (PyArrayObject*)PyArray_FROM_OTF(ret,NPY_DOUBLE, NPY_ARRAY_OUT_ARRAY);
   npy_intp index[3];
-  idx =0;
-  index[2] = 0;
+  int idx =0;
+  index[2] = 0; 
+
   for(int i=0;i<dimY;i++)
-    for(int j=0;j<dimX;j++)
+  {
+    for(int j=0;j<dimX;j++) 
     {
-      index[0] = i;
-      index[1] = j;
+      index[0] = j; 
+      index[1] = i;
       if(ubuff[idx] == color)
           { 
             
@@ -93,24 +107,25 @@ static PyObject * slic_NumpyArgs(PyObject *self, PyObject *args)
                 printf("[slicmodule.cpp] Error while setting items in output array.\n");
                 return NULL;
               }
-              // else
-              //   printf("SETITEM OK in PyArray at location i : %d  j : %d \n",i,j );
           }
           else
+          {
             if(PyArray_SETITEM((PyArrayObject*)returnval, (char*)PyArray_GetPtr(returnval,index), (PyObject*)Py_BuildValue("d",COLORBACKGROUND)) < 0)
               {  
                 printf("[slicmodule.cpp] Error while setting items in output array.\n");
                 return NULL;
               }
-
-
+          }
       idx++;
     }
 
+  }
+  
   #ifdef DEBUG
   printf("[slicmodule.cpp] Output array ready, casting PyArray to PyObject\n");
   #endif 
 
+  Py_INCREF(returnval);
   return (PyObject*)returnval;
 }
 
